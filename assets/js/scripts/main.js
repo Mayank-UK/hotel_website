d257baec4fa65aa680f8df3b2321ab48ed68120a
Mayank-UK <mayankupadhyay191298@gmail.com>
2021-03-12 23:11:55 +0530

    only remaining gallery and booking system logic

--- assets/js/scripts/main.js
+++ assets/js/scripts/main.js
@@ -36,41 +36,6 @@ $(window).on('load', function () {
 		return {};
 	})();
 
-	// contact form module
-	var contactForm = (function () {
-		// cache DOM
-		var submitButton = $('#j-contact-submit');
-
-		// bind events
-		submitButton.click(_submitForm);
-
-		function _submitForm(e) {
-			e.preventDefault();
-			alert('this is demo form');
-		}
-
-		// return
-		return {};
-	})();
-
-	// smooth scrolling module
-	var smoothScroll = (function () {
-		// cache DOM
-		var smoothScrollTarget = $('a[href*="#"]');
-
-		// internal methods
-		_addSmoothScrolling();
-
-		function _addSmoothScrolling() {
-			smoothScrollTarget.smoothscroll({
-				duration: 300,
-			});
-		}
-
-		// return
-		return {};
-	})();
-
 	// active navigation element module
 	var activeNavigation = (function () {
 		// properties
@@ -78,7 +43,7 @@ $(window).on('load', function () {
 		var currentClassDesktop = 'm-nav-desktop__anchor--active';
 
 		// cache DOM
-		var sections = $('.m-main-section');
+		var page = $('.l-main')[0];
 		var anchorLinksMobile = $('.m-nav-mobile__anchor');
 		var anchorLinksDesktop = $('.m-nav-desktop__anchor');
 
@@ -86,52 +51,22 @@ $(window).on('load', function () {
 		_activeNavigationLinkMobile();
 		_activeNavigationLinkDesktop();
 
+		function _markActiveLink() {
+			if (
+				$(page).attr('data-active-page') ==
+				$(this).attr('data-active-link')
+			) {
+				$(this).addClass(currentClassMobile);
+				$(this).addClass(currentClassDesktop);
+			}
+		}
+
 		function _activeNavigationLinkMobile() {
-			sections.headerhighlight({
-				elementsChanging: anchorLinksMobile,
-				currentClass: currentClassMobile,
-				offset: 250,
-			});
+			anchorLinksMobile.each(_markActiveLink);
 		}
 
 		function _activeNavigationLinkDesktop() {
-			sections.headerhighlight({
-				elementsChanging: anchorLinksDesktop,
-				currentClass: currentClassDesktop,
-				offset: 250,
-			});
-		}
-
-		// return
-		return {};
-	})();
-
-	// scroll animations module
-	var animateOnScroll = (function () {
-		// internal methods
-		_aosInit();
-
-		function _aosInit() {
-			AOS.init({
-				// Global settings:
-				disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-				startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
-				initClassName: 'aos-init', // class applied after initialization
-				animatedClassName: 'aos-animate', // class applied on animation
-				useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-				disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-				debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-				throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
-
-				// Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-				offset: 120, // offset (in px) from the original trigger point
-				delay: 0, // values from 0 to 3000, with step 50ms
-				duration: 400, // values from 0 to 3000, with step 50ms
-				easing: 'ease', // default easing for AOS animations
-				once: true, // whether animation should happen only once - while scrolling down
-				mirror: false, // whether elements should animate out while scrolling past them
-				anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
-			});
+			anchorLinksDesktop.each(_markActiveLink);
 		}
 
 		// return

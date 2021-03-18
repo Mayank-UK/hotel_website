66521d1d76531f884c8ec59c9873fc51074b3829
Mayank-UK <mayankupadhyay191298@gmail.com>
2021-03-18 13:55:00 +0530

    completed

--- assets/js/scripts/main.js
+++ assets/js/scripts/main.js
@@ -12,6 +12,34 @@ $(window).on('load', function () {
 	})();
 	 */
 
+	var disclaimer = (function () {
+		// properties
+		var togglableClass = 'l-modal--hide';
+
+		// cache DOM
+		var modal = $('#j-modal');
+		var modalButton = $('#j-modal-button');
+
+		// bind events
+		modalButton.click(__toggleModalVisibility);
+
+		function __toggleModalVisibility() {
+			modal.addClass(togglableClass);
+		}
+
+		// internal methods
+		__showModal();
+
+		function __showModal() {
+			setTimeout(function () {
+				modal.removeClass(togglableClass);
+			}, 2000);
+		}
+
+		// return
+		return {};
+	})();
+
 	// mobile navigation module
 	var mobileNav = (function () {
 		// properties
@@ -67,6 +95,98 @@ $(window).on('load', function () {
 
 		function _activeNavigationLinkDesktop() {
 			anchorLinksDesktop.each(_markActiveLink);
+		}
+
+		// return
+		return {};
+	})();
+
+	// gallery
+	var gallery = (function () {
+		// properties
+
+		// cache DOM
+		var gallery = $('#gallery');
+
+		// internal methods
+		_initGallery();
+
+		function _initGallery() {
+			if (gallery.length) {
+				gallery.unitegallery();
+			}
+		}
+
+		// return
+		return {};
+	})();
+
+	// booking
+	var booking = (function () {
+		// properties
+		var activeLink = 'm-booking-paging__item--active';
+		var activeSection = 'l-booking__section--active';
+
+		// cache DOM
+		var tabLinks = $('[data-tab-link]');
+		var tabSections = $('[data-tab-section]');
+		var submitButton = $('#l-booking__review-button');
+
+		// bind events
+		tabLinks.on('click', _activeTabPanel);
+		submitButton.on('click', _formSubmit);
+
+		function _activeTabPanel() {
+			tabLinks.each(function () {
+				$(this).removeClass(activeLink);
+			});
+
+			tabSections.each(function () {
+				$(this).removeClass(activeSection);
+			});
+
+			$(this).addClass(activeLink);
+			var currentLink = $(this);
+
+			tabSections.each(function () {
+				if (
+					$(this).attr('data-tab-section') ==
+					currentLink.attr('data-tab-link')
+				) {
+					$(this).addClass(activeSection);
+				}
+			});
+		}
+
+		function _formSubmit(e) {
+			e.preventDefault();
+
+			alert('this is a demo form');
+		}
+
+		// internal methods
+
+		// return
+		return {};
+	})();
+
+	var datePicker = (function () {
+		// properties
+
+		// cache DOM
+		var dateInputField = $('#date');
+
+		// bind events
+
+		// internal methods
+		_initDatePicker();
+
+		function _initDatePicker() {
+			if (dateInputField.length) {
+				dateInputField.dateRangePicker({
+					startDate: $(this),
+				});
+			}
 		}
 
 		// return
